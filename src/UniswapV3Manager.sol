93c15189d522c0545427dbd5ae534e011a483640
gp-ngmi <gp.bc.github@gmail.com>
2024-05-17 22:55:43 +0200

    Milestone 3. - Cross-Tick Swaps - Refactor

--- src/UniswapV3Manager.sol
+++ src/UniswapV3Manager.sol
@@ -1,25 +1,16 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.14;
 
-import "./UniswapV3Pool.sol";
-
 import "./interfaces/IERC20.sol";
-import "../src/interfaces/IUniswapV3Pool.sol";
-import "../src/lib/LiquidityMath.sol";
-import "../src/lib/TickMath.sol";
+import "./interfaces/IUniswapV3Pool.sol";
+import "./interfaces/IUniswapV3Manager.sol";
 
-contract UniswapV3Manager {
+import "./lib/LiquidityMath.sol";
+import "./lib/TickMath.sol";
+
+contract UniswapV3Manager is IUniswapV3Manager {
     error SlippageCheckFailed(uint256 amount0, uint256 amount1);
 
-    struct MintParams {
-        address poolAddress;
-        int24 lowerTick;
-        int24 upperTick;
-        uint256 amount0Desired;
-        uint256 amount1Desired;
-        uint256 amount0Min;
-        uint256 amount1Min;
-    }
     function mint(MintParams calldata params)
         public
         returns (uint256 amount0, uint256 amount1)
@@ -83,9 +74,9 @@ contract UniswapV3Manager {
     }
 
     function uniswapV3MintCallback(uint256 amount0, uint256 amount1, bytes calldata data) public {
-            UniswapV3Pool.CallbackData memory extra = abi.decode(
+            IUniswapV3Pool.CallbackData memory extra = abi.decode(
                 data,
-                (UniswapV3Pool.CallbackData)
+                (IUniswapV3Pool.CallbackData)
             );
 
             IERC20(extra.token0).transferFrom(extra.payer, msg.sender, amount0);
@@ -95,9 +86,9 @@ contract UniswapV3Manager {
 
     function uniswapV3SwapCallback(int256 amount0, int256 amount1, bytes calldata data) public {
 
-             UniswapV3Pool.CallbackData memory extra = abi.decode(
+            IUniswapV3Pool.CallbackData memory extra = abi.decode(
                 data,
-                (UniswapV3Pool.CallbackData)
+                (IUniswapV3Pool.CallbackData)
             );
 
             if (amount0 > 0) {
--- src/UniswapV3Pool.sol
+++ src/UniswapV3Pool.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.14;
 import "./interfaces/IERC20.sol";
 import "./interfaces/IUniswapV3FlashCallback.sol";
 import "./interfaces/IUniswapV3MintCallback.sol";
+import "./interfaces/IUniswapV3Pool.sol";
 import "./interfaces/IUniswapV3SwapCallback.sol";
 
 import "./lib/LiquidityMath.sol";
@@ -14,7 +15,7 @@ import "./lib/TickBitmap.sol";
 import "./lib/TickMath.sol";
 import "./lib/SwapMath.sol";
 
-contract UniswapV3Pool {
+contract UniswapV3Pool is IUniswapV3Pool {
     using Position for mapping(bytes32 => Position.Info);
     using Position for Position.Info;
     using Tick for mapping(int24 => Tick.Info);
@@ -61,12 +62,6 @@ contract UniswapV3Pool {
         uint160 sqrtPriceX96;
         // Current tick
         int24 tick;
-    }
-
-    struct CallbackData {
-        address token0;
-        address token1;
-        address payer;
     }
 
     struct SwapState {
