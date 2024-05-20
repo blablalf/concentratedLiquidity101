9afd94e020a442efd57d716ee51cb85b4e4ac51a
gp-ngmi <gp.bc.github@gmail.com>
2024-05-21 01:33:42 +0200

    Milestone 6:  - NFT Positions - NFT Manager

--- src/interfaces/IUniswapV3Pool.sol
+++ src/interfaces/IUniswapV3Pool.sol
@@ -48,6 +48,20 @@ interface IUniswapV3Pool {
         bytes calldata data
     ) external returns (uint256 amount0, uint256 amount1);
 
+    function burn(
+        int24 lowerTick,
+        int24 upperTick,
+        uint128 amount
+    ) external returns (uint256 amount0, uint256 amount1);
+
+    function collect(
+        address recipient,
+        int24 lowerTick,
+        int24 upperTick,
+        uint128 amount0Requested,
+        uint128 amount1Requested
+    ) external returns (uint128 amount0, uint128 amount1);
+    
     function swap(
         address recipient,
         bool zeroForOne,
